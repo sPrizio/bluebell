91a8a142fec9bbcb2b56ee6375d89858c8fbb2b1
Stephen <s.prizio@hotmail.com>
2025-02-23 17:18:46 -0500

    feature/v0.0.9 adds cleaner functionality

---
+++ greenhouse/processing/src/cleanup/java/com/bluebell/processing/cleaners/Cleaner.java
@@ -0,0 +1,18 @@
+package com.bluebell.processing.cleaners;
+
+/**
+ * A cleaner is a class that is responsible for cleaning up generated sources after code compilation
+ * and before new compilation
+ *
+ * @author Stephen Prizio
+ * @version 0.0.9
+ */
+public interface Cleaner {
+
+    /**
+     * Executes the cleaner
+     *
+     * @param args system arguments
+     */
+    static void main(String[] args) {}
+}
