da05f9011d4a549d9755f7f337ea9d07bd1f08d1
Stephen <s.prizio@hotmail.com>
2025-03-12 00:19:22 -0400

    feature/BB-22 refactors job enums

--- greenhouse/src/main/java/com/bluebell/platform/enums/job/JobStatus.java
+++ greenhouse/src/main/java/com/bluebell/platform/enums/job/JobStatus.java
@@ -1,6 +1,8 @@
 package com.bluebell.platform.enums.job;
 
 import com.bluebell.platform.models.core.entities.job.impl.Job;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
 
 /**
  * Enum representing various states that a {@link Job} can be in
@@ -8,9 +10,39 @@ import com.bluebell.platform.models.core.entities.job.impl.Job;
  * @author Stephen Prizio
  * @version 0.1.1
  */
+@Getter
+@Schema(title = "JobStatus", name = "JobStatus", description = "The various states that a Job can be in.")
 public enum JobStatus {
-    NOT_STARTED,
-    IN_PROGRESS,
-    COMPLETED,
-    FAILED
+    NOT_STARTED("", ""),
+    IN_PROGRESS("", ""),
+    COMPLETED("", ""),
+    FAILED("", "");
+
+    private final String code;
+
+    private final String label;
+
+    JobStatus(final String code, final String label) {
+        this.code = code;
+        this.label = label;
+    }
+
+
+    //  METHODS
+
+    /**
+     * Get enum by code
+     *
+     * @param code input code
+     * @return {@link JobStatus}
+     */
+    public static JobStatus getByCode(final String code) {
+        return switch (code) {
+            case "NOT_STARTED" -> NOT_STARTED;
+            case "IN_PROGRESS" -> IN_PROGRESS;
+            case "COMPLETED" -> COMPLETED;
+            case "FAILED" -> FAILED;
+            default -> null;
+        };
+    }
 }
