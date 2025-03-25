f13b63dfc0027d13a73c8dcd61ae99dca2e05435
Stephen <s.prizio@hotmail.com>
2025-03-25 14:45:50 -0400

    bugfix/BB-14 fixes script line endings

--- scripts/linux/prune-git-branches.sh
+++ scripts/linux/prune-git-branches.sh
@@ -4,4 +4,4 @@ ROOT_FOLDER=$(realpath "$0" | xargs dirname | xargs dirname)
 cd "$ROOT_FOLDER" || exit
 
 git fetch -p
-git branch -vv | grep ': gone' | awk '{print $1}' #| xargs git branch -D
+git branch -vv | grep ': gone' | awk '{print $1}' | xargs git branch -D
