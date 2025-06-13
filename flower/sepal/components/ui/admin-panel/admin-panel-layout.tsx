366137c36b662575ed7bdda595bae9bd36e6b4f7
Stephen <s.prizio@hotmail.com>
2025-06-13 12:52:47 -0400

    adds build version to nav bar

--- flower/sepal/components/ui/admin-panel/admin-panel-layout.tsx
+++ flower/sepal/components/ui/admin-panel/admin-panel-layout.tsx
@@ -5,7 +5,15 @@ import { useSidebar } from "@/lib/hooks/ui/use-sidebar";
 import { useStore } from "@/lib/hooks/ui/use-store";
 import { cn } from "@/lib/utils";
 import Footer from "@/components/Footer/Footer";
+import React from "react";
 
+/**
+ * Layout for the admin panel view (default sepal view)
+ *
+ * @param children react content
+ * @author Stephen Prizio
+ * @version 0.2.5
+ */
 export default function AdminPanelLayout({
   children,
 }: Readonly<{
@@ -14,12 +22,15 @@ export default function AdminPanelLayout({
   const sidebar = useStore(useSidebar, (x) => x);
   if (!sidebar) return null;
   const { getOpenState, settings } = sidebar;
+
+  //  RENDER
+
   return (
     <>
       <Sidebar />
       <main
         className={cn(
-          "min-h-[calc(100vh_-_56px)] bg-[#f6f8fb] transition-[margin-left] ease-in-out duration-300",
+          "min-h-[calc(95vh_-_56px)] bg-[#f6f8fb] transition-[margin-left] ease-in-out duration-300",
           !settings.disabled && (!getOpenState() ? "lg:ml-[90px]" : "lg:ml-72"),
         )}
       >
--- flower/sepal/components/ui/admin-panel/navbar.tsx
+++ flower/sepal/components/ui/admin-panel/navbar.tsx
@@ -4,7 +4,7 @@ interface NavbarProps {
   title: string;
 }
 
-export function Navbar({ title }: NavbarProps) {
+export function Navbar({ title }: Readonly<NavbarProps>) {
   return (
     <header className="sticky top-0 z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary">
       <div className="mx-4 sm:mx-8 flex h-14 items-center">
