0b6fa853d8d801e97b8553fdc9e46538f46a7c29
Stephen <s.prizio@hotmail.com>
2025-06-25 11:04:07 -0400

    feature/BB-31 updates release.yaml

--- scripts/linux/prune-git-branches.sh
+++ scripts/linux/prune-git-branches.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+set -euo pipefail
+
 ROOT_FOLDER=$(realpath "$0" | xargs dirname | xargs dirname)
 cd "$ROOT_FOLDER" || exit
 
 git fetch -p
-git branch -vv | grep ': gone' | awk '{print $1}' | xargs git branch -D
+git branch -vv | grep ': gone]' | awk '{print $1}' | xargs -r git branch -D
+
+remote_tags=$(git ls-remote --tags origin | awk '{print $2}' | sed 's#refs/tags/##' | sed 's/\^{}//')
+
+local_tags=$(git tag)
+
+for tag in $local_tags; do
+  if ! grep -Fxq "$tag" <<< "$remote_tags"; then
+    git tag -d "$tag"
+  fi
+done
--- scripts/windows/prune-git-branches.bat
+++ scripts/windows/prune-git-branches.bat
@@ -6,3 +6,14 @@ cd /d %ROOT_FOLDER% || exit
 
 git fetch -p
 for /f "tokens=1" %%A in ('git branch -vv ^| findstr ": gone"') do git branch -D %%A
+
+git ls-remote --tags origin > remote_tags.tmp
+
+for /f %%T in ('git tag') do (
+    findstr /C:"refs/tags/%%T" remote_tags.tmp >nul
+    if errorlevel 1 (
+        git tag -d %%T
+    )
+)
+
+del remote_tags.tmp
